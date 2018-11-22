4eda9faedac099674949ecdbbeb9075c35f29ed4
Terry-zh <496971418@qq.com>
2018-11-22 23:15:29 +0800

    [add] map demo

--- src/queue/entry/entry.go
+++ src/queue/entry/entry.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"queue"
+	"fmt"
+)
 
 func main() {
+	q := queue.Queue{1}
+	fmt.Println(q)
+	q.Push(2)
+	q.Push(3)
+	q.Pop()
+	fmt.Println(q)
+	q.Pop()
+	fmt.Println(q)
+	q.Pop()
+	fmt.Println(q.IsEmpty())
 }
